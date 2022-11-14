d61d529ef6ffcf31707f4c45fdbfdf8bfcbf3eae
Almog Simchon <almog.si@gmail.com>
2022-11-14 19:27:29 +0000

    Update mediation.r

--- code/analysis/mediation.r
+++ code/analysis/mediation.r
@@ -8,46 +8,40 @@ if (!require("pacman")) install.packages("pacman")
 pacman::p_load(tidyverse, mediation)
 
 #load data
-liwc_binded <- read_csv("data/tweets/US_politician_tweets_2010-11-06_to_2022-03-16.csv.gzip")
+data_users <- read_csv("data/users/US_politician_accounts_2010-11-06_to_2022-03-16.csv")
 
-
-#summarize
-liwc_summarized <- liwc_binded %>% 
-  group_by(author_id, party) %>% 
-  summarise(neg = mean(LIWC_emo_neg, na.rm=T),
-            belief_share = mean(belief, na.rm=T),
-            truth_share = mean(truth, na.rm=T),
-            fishy = mean(NG_unreliable, na.rm=T)) %>%
-  ungroup() %>% 
-  filter(party %in% c("Republican", "Democrat"))
+data_users <- data_users %>% 
+  dplyr::select(author_id, party, NG_score_mean,
+                LIWC_emo_neg_mean, avg_belief_score, avg_truth_score) %>% 
+  drop_na()
 
 
 #Negative emotion as mediator
 
 #Reps
-path.y.rep.neg <- lm(data = na.omit(filter(liwc_summarized, party == "Republican")),
-                     fishy~neg+belief_share)
+path.y.rep.neg <- lm(data = filter(data_users, party == "Republican"),
+                     NG_score_mean~LIWC_emo_neg_mean+avg_belief_score)
 
-path.m.rep.neg <- lm(data = na.omit(filter(liwc_summarized, party == "Republican")),
-                     neg~belief_share)
+path.m.rep.neg <- lm(data = na.omit(filter(data_users, party == "Republican")),
+                     LIWC_emo_neg_mean~avg_belief_score)
 
 med.belief.rep.neg <- mediation::mediate(path.m.rep.neg ,path.y.rep.neg , 
-                                         treat = "belief_share", mediator = "neg" ,
+                                         treat = "avg_belief_score", mediator = "LIWC_emo_neg_mean" ,
                                          boot = T, sims = 10000)
 
 #Summary
 summary(med.belief.rep.neg)
-
+plot(med.belief.rep.neg)
 
 #Dems
-path.y.dem.neg <- lm(data = na.omit(filter(liwc_summarized, party == "Democrat")),
-                     fishy~neg+belief_share)
+path.y.dem.neg <- lm(data = na.omit(filter(data_users, party == "Democrat")),
+                     NG_score_mean~LIWC_emo_neg_mean+avg_belief_score)
 
-path.m.dem.neg <- lm(data = na.omit(filter(liwc_summarized, party == "Democrat")),
-                     neg~belief_share)
+path.m.dem.neg <- lm(data = na.omit(filter(data_users, party == "Democrat")),
+                     LIWC_emo_neg_mean~avg_belief_score)
 
 med.belief.dem.neg <- mediation::mediate(path.m.dem.neg ,path.y.dem.neg , 
-                                         treat = "belief_share", mediator = "neg" ,
+                                         treat = "avg_belief_score", mediator = "LIWC_emo_neg_mean" ,
                                          boot = T, sims = 10000)
 
 #Summary
@@ -59,29 +53,29 @@ summary(med.belief.dem.neg)
 #Belief Speaking as mediator
 
 #Reps
-path.y.rep <- lm(data = na.omit(filter(liwc_summarized, party == "Republican")),
-                 fishy~neg+belief_share)
+path.y.rep <- lm(data = na.omit(filter(data_users, party == "Republican")),
+                 NG_score_mean~LIWC_emo_neg_mean+avg_belief_score)
 
-path.m.rep <- lm(data = na.omit(filter(liwc_summarized, party == "Republican")),
-                 belief_share~neg)
+path.m.rep <- lm(data = na.omit(filter(data_users, party == "Republican")),
+                 avg_belief_score~LIWC_emo_neg_mean)
 
 med.belief.rep <- mediation::mediate(path.m.rep ,path.y.rep , 
-                                     treat = "neg", mediator = "belief_share",
+                                     treat = "LIWC_emo_neg_mean", mediator = "avg_belief_score",
                                      boot = T, sims = 10000)
 
 #Summary
 summary(med.belief.rep)
-
+plot(med.belief.rep)
 
 #Dems
-path.y.dem <- lm(data = na.omit(filter(liwc_summarized, party == "Democrat")),
-                 fishy~neg+belief_share)
+path.y.dem <- lm(data = na.omit(filter(data_users, party == "Democrat")),
+                 NG_score_mean~LIWC_emo_neg_mean+avg_belief_score)
 
-path.m.dem <- lm(data = na.omit(filter(liwc_summarized, party == "Democrat")),
-                 belief_share~neg)
+path.m.dem <- lm(data = na.omit(filter(data_users, party == "Democrat")),
+                 avg_belief_score~LIWC_emo_neg_mean)
 
 med.belief.dem <- mediation::mediate(path.m.dem ,path.y.dem , 
-                                     treat = "neg", mediator = "belief_share",
+                                     treat = "LIWC_emo_neg_mean", mediator = "avg_belief_score",
                                      boot = T, sims = 10000)
 
 #Summary
