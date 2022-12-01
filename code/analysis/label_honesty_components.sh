96b8f3407b57d1d319bec4bdd354404f5ea472db
Jana <jana.lasser@ds.mpg.de>
2022-12-01 14:07:05 +0100

    cleanup

--- code/analysis/label_honesty_components.sh
+++
@@ -1,6 +0,0 @@
-# author: Segun Aroyehun
-
-python sample.py --input_file combined_US_politician_twitter_timelines_2010-11-06_to_2022-03-16_clean.csv.gzip \
-	--output_file combined_US_politician_twitter_timelines_2010-11-06_to_2022-03-16_honesty_component_labels.csv \
-	--threshold 1 \
-	--corpus Twitter 
