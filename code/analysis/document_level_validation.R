b8847ff4681c8c00acbcb354c47e0f5da5659fb9
Jana <jana.lasser@ds.mpg.de>
2023-03-11 16:17:39 +0100

    analysis of the document validation data

--- code/analysis/document_level_validation.R
+++ code/analysis/document_level_validation.R
@@ -1,8 +1,7 @@
 # import libraries and data
-library(tidyverse)
-library(broom)
 library(verification)
-library(pROC)
+library(tidyverse)
+library(caret)
 
 Document_Level_Honesty_Validation <- read_csv("../../data/validation/document_validation_data.csv")
 
@@ -23,26 +22,39 @@ df_long <- attention_check_df %>%
   id = as.numeric(gsub("^.{0,7}", "", doc))) %>% 
   filter(id != 30) %>% 
   mutate(id = ifelse(id > 30, id - 1, id))
-  
 
 
-# make data wide
+# make data wide and count components rates > 3
 df_wide <- df_long %>% 
   pivot_wider(names_from = rate_type, values_from = rate) %>% 
   group_by(ResponseId, id) %>% 
   summarise(belief = sum(belief, na.rm = T),
             truth = sum(truth, na.rm = T))
 
-
-# calculate t values
-df_t_test <- df_wide %>% 
+belief_count <-  df_wide %>% 
   filter(belief + truth != 0) %>% 
+  mutate(is_belief = ifelse(belief %in% c(4,5), 1, 0)) %>% 
   group_by(id) %>% 
-  summarise(t = t.test(belief, truth, paired=T)$statistic,
-            pval = t.test(belief, truth, paired=T)$p.value,
-            belief_survey_avg = mean(belief),
-            truth_survey_avg = mean(truth)) %>% 
-  mutate(across(c(4:5), ~scales::rescale(.x, to = c(0,1))))
+  count(is_belief) %>% 
+  pivot_wider(names_from = is_belief, names_prefix = "belief_", values_from = n) %>% 
+  mutate(is_belief = ifelse(belief_1 > belief_0, 1, 0))
+
+truth_count <-  df_wide %>% 
+  filter(belief + truth != 0) %>% 
+  mutate(is_truth = ifelse(truth %in% c(4,5), 1, 0)) %>% 
+  group_by(id) %>% 
+  count(is_truth) %>% 
+  pivot_wider(names_from = is_truth, names_prefix = "truth_", values_from = n) %>% 
+  mutate(is_truth = ifelse(truth_1 > truth_0, 1, 0))
+
+df_count <- belief_count %>% 
+  inner_join(truth_count, by = "id") %>% 
+  mutate(survey_cat_count = case_when(
+    is_belief == 1 ~ "high_belief",
+    is_truth == 1 ~ "high_truth",
+    TRUE ~ "low_honesty"
+  )) %>% 
+  dplyr::select(id, survey_cat_count) 
 
 
 # edit tweet sample categories
@@ -54,23 +66,40 @@ tweet_sample <- document_validation_sample %>%
   ))
 
 
-# join tweets with t test values
+# join tweets with count values
 sample_t_test <- tweet_sample %>% 
-  mutate(n_row = row_number()) %>% 
-  left_join(df_t_test, by = c("n_row" = "id")) %>% 
-  dplyr::select(-n_row) %>% 
-  mutate(survey_cat = case_when(
-    pval < 0.05 & t > 0 ~ "high_belief",
-    pval < 0.05 & t < 0 ~ "high_truth",
-    TRUE ~ "low_honesty"
-  ))
+  mutate(n_row = row_number()) %>%
+  left_join(df_count, by = c("n_row" = "id")) %>% 
+  dplyr::select(-n_row)
+
+library(caret)
+
+# confusion matrix
+confusionMatrix(data = as.factor(sample_t_test$category),
+                reference = as.factor(sample_t_test$survey_cat_count))
+
+#ROC curves
+belief_roc <- sample_t_test %>% 
+  mutate(survey_roc_count = ifelse(survey_cat_count != "high_belief", 0, 1),
+         belief_ddr = scales::rescale(avg_belief_score, to = c(0,1)))
+
+truth_roc <- sample_t_test %>% 
+  mutate(survey_roc_count = ifelse(survey_cat_count != "high_truth", 0, 1),
+         truth_ddr = scales::rescale(avg_truth_score, to = c(0,1)))
+
+roc.plot(belief_roc$survey_roc_count, belief_roc$belief_ddr, 
+         main = "Belief-speaking ROC Curve", plot = "both", binormal = T, CI = T)
+roc.plot(truth_roc$survey_roc_count, truth_roc$truth_ddr,
+         main = "Truth-seeking ROC Curve", plot = "both", binormal = T, CI = T)
+
+roc.area(belief_roc$survey_roc_count, belief_roc$belief_ddr)
+roc.area(truth_roc$survey_roc_count, truth_roc$truth_ddr)
 
 
 # plot histograms
 ggplot(sample_t_test) +
-  aes(x = survey_cat, fill = survey_cat) +
+  aes(x = survey_cat_count, fill = survey_cat_count) +
   geom_bar() +
-  scale_fill_hue(direction = 1) +
   facet_wrap(vars(category)) +
   scale_fill_manual(
     values = c(high_belief = "#FF6E40",
@@ -80,7 +109,7 @@ ggplot(sample_t_test) +
 # plot boxplots
 boxplot_df <- tweet_sample %>% 
   mutate(doc_id = row_number()) %>% 
-  right_join(df_long, by = c("doc_id" = "id"))
+  right_join(df_long, by = c("doc_id" = "id"), multiple = "all")
   
 boxplot_df %>%
   filter(category %in% "high_belief") %>%
@@ -97,8 +126,6 @@ boxplot_df %>%
   xlab("Doc ID") +
   ylab("Rate Distributions")
 
-ggsave("../../plots/SI_fig4.pdf", width=8, height=6)
-
 boxplot_df %>%
   filter(category %in% "high_truth") %>%
   ggplot() +
@@ -114,8 +141,6 @@ boxplot_df %>%
   xlab("Doc ID") +
   ylab("Rate Distributions")
 
-ggsave("../../plots/SI_fig5.pdf", width=8, height=6)
-
 boxplot_df %>%
   filter(category %in% "low_honesty") %>%
   ggplot() +
@@ -130,23 +155,3 @@ boxplot_df %>%
   labs(fill = "Rate Type", title = "Low Honesty Tweets") +
   xlab("Doc ID") +
   ylab("Rate Distributions")
-
-ggsave("../../plots/SI_fig6.pdf", width=8, height=6)
-
-# ROC curves
-belief_roc <- sample_t_test %>% 
-  mutate(sample_roc = ifelse(category != "high_belief", 0, 1),
-         survey_roc = ifelse(survey_cat != "high_belief", 0, 1))
-
-truth_roc <- sample_t_test %>% 
-  mutate(sample_roc = ifelse(category != "high_truth", 0, 1),
-         survey_roc = ifelse(survey_cat != "high_truth", 0, 1))
-
-roc.plot(belief_roc$sample_roc, belief_roc$belief_survey_avg, 
-                       main = "Belief-speaking ROC Curve", plot = "both", binormal = T, CI = T)
-
-roc.plot(truth_roc$sample_roc, truth_roc$truth_survey_avg,
-                       main = "Truth-seeking ROC Curve", plot = "both", binormal = T, CI = T)
-
-roc.area(belief_roc$sample_roc, belief_roc$belief_survey_avg)
-roc.area(truth_roc$sample_roc, truth_roc$truth_survey_avg)
