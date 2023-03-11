960ebb9b21f4b5608b666148baf09a9f619081bc
Jana <jana.lasser@ds.mpg.de>
2023-03-11 16:15:40 +0100

    updated with modified embedding

--- code/analysis/label_glove840B_DDR.sh
+++ code/analysis/label_glove840B_DDR.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 source_path="./"
-model_name_or_path="../../data/utilities/sentence-transformers/average_word_embeddings_glove.840B.300d"
+model_name_or_path="../../data/utilities/sentence-transformers/glove-model-reduced-stopwords"
 
 # compute belief-speaking and truth-seeking similarities for the twitter corpus 
 # using the glove embedding
 python ${source_path}/compute_sbert_avg_lexicon.py --model_name_or_path ${model_name_or_path}\
-	--input_file "../../data/tweets/combined_US_politician_twitter_timelines_2010-11-06_to_2022-03-16_clean.csv.gzip"\
-	--output_file "../../data/tweets/combined_US_politician_twitter_timelines_2010-11-06_to_2022-03-16_honesty_component_scores_glove.csv.gzip"\
+	--input_file "../../data/tweets/combined_US_politician_twitter_timelines_2010-11-06_to_2022-12-31_clean.csv.gzip"\
+	--output_file "../../data/tweets/combined_US_politician_twitter_timelines_2010-11-06_to_2021-12-31_honesty_component_scores_glove.csv.gzip"\
 	--truth_lexicon "../../data/utilities/truth_seeking_p=0.05_swapped_wn_def_example.csv"\
 	--belief_lexicon "../../data/utilities/belief_speaking_p=0.05_swapped_wn_def_example.csv"\
 	--avg_dict --average_of_similarity\
-    --compression_type "gzip"\
+   --compression_type "gzip"\
 	--corpus "Twitter"\
-    #--smoke_test
+   #--smoke_test
 
 # compute belief-speaking and truth-seeking similarities for the articles corpus
 # using the glove embedding
@@ -42,6 +42,6 @@ python ${source_path}/compute_sbert_avg_lexicon.py --model_name_or_path {model_n
 	--truth_lexicon "../../data/utilities/truth_seeking_p=0.05_swapped_wn_def_example.csv"\
 	--belief_lexicon "../../data/utilities/belief_speaking_p=0.05_swapped_wn_def_example.csv"\
 	--avg_dict --average_of_similarity\
-   --compression_type "gzip"\
-   --corpus "NYT"\
+    --compression_type "gzip"\
+    --corpus "NYT"\
     #--smoke_test
